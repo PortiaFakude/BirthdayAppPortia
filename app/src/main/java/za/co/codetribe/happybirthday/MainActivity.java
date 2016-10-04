ed01b6f4def0dd86ad03bb1fb72823048b82e41e
Portia <portiafakude@gmail.com>
2016-10-04 11:02:07 +0200

    Initial commit

---
+++ app/src/main/java/za/co/codetribe/happybirthday/MainActivity.java
@@ -0,0 +1,23 @@
+package za.co.codetribe.happybirthday;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void show(View view)
+    {
+        Intent i = new Intent(MainActivity.this,SecondActivity.class);
+
+        startActivity(i);
+
+    }
+}
